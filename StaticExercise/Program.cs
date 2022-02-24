9a6c655c58179087a4066352af63b47b861c5d7e
Zachary Whitley <zbwhitley@gmail.com>
2022-02-24 10:45:07 -0600

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("This is a temperature converter program.");
+            Console.WriteLine("If you need to convert Fahrenheit to Celsius please enter 1.");
+            Console.WriteLine("If you need to convert Celsius to Fahrenheit please enter 2:");
+            Start:
+            int selector;
+            string input = Console.ReadLine();
+            bool isString = int.TryParse(input, out selector);
+
+            if (selector == 1)
+            {
+                Console.WriteLine("Please input your temperature in Fahrenheit:");
+                double v = Convert.ToDouble(Console.ReadLine());
+                double cel = TempConverter.FahrenheitToCelsius(v);
+                Console.WriteLine($"Your temperature in Celsius is {cel} degrees.");
+                return;
+            }
+            else if (selector == 2)
+            {
+                Console.WriteLine("Please input your temperature in Celsius:");
+                double v = Convert.ToDouble(Console.ReadLine());
+                double fah = TempConverter.CelsiusToFahrenheit(v);
+                Console.WriteLine($"Your temperature in Fahrenheit is {fah} degrees.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"You used an invalid input, please input a 1 or 2:");
+                goto Start;
+            }
+            
+            
+            
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        
+        public static double FahrenheitToCelsius(double v) 
+        {         
+            return (v - 32) / 1.8;
+        }
+
+        public static double CelsiusToFahrenheit(double v) 
+        {
+            return ((v * 9) / 5) + 32;
+            
+        }
+    }
+}
